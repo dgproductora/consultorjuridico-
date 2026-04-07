3606af2e930ee5185ff68e675314e1e1e61ff597
dgproductora <dgproductora@gmail.com>
2026-04-07 10:02:22 -0300

    Actualización
    7-4-2026

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getMessaging, isSupported } from 'firebase/messaging';
+import firebaseConfig from './firebase-applet-config.json';
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const auth = getAuth(app);
+
+export const getMessagingInstance = async () => {
+  try {
+    const supported = await isSupported();
+    if (supported) {
+      return getMessaging(app);
+    }
+  } catch (error) {
+    console.error("FCM is not supported in this environment", error);
+  }
+  return null;
+};
